0d26febd68a383c39380ce836a63a1b47be4adff
Deiby_P <98618493+deiby1523@users.noreply.github.com>
2023-11-01 23:35:51 -0500

    Optimization in Login

--- resources/views/auth/example_Login.blade.php
+++ resources/views/auth/example_Login.blade.php
@@ -42,7 +42,7 @@
 <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3 navbar-transparent">
     <div class="container">
         <a class="navbar-brand  text-white " href="#" rel="tooltip" title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">
-            Ecommerce Example
+            Ecommerce Example.
         </a>
         <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon mt-2">
