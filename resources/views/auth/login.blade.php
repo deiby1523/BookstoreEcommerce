48c648533fbee639830dbe12c7319f1777dfdf50
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-11-26 16:38:09 -0500

    Color modifications were made to the login form.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,32 @@
+<html>
 <head>
     <title>Hello, world!</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 
-    <link href={{asset('nucleo-icons.css')}} rel="stylesheet" />
-    <link href={{asset('css/nucleo-svg.css')}} rel="stylesheet" />
     <!--     Fonts and icons     -->
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-    <link rel="stylesheet" href={{asset('icons/icons.css')}}>
+    <link rel="stylesheet" type="text/css"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
 
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <!-- Material Kit CSS -->
-    <link href={{asset('css/material-kit.css')}} rel="stylesheet" />
+    <link href={{asset('css/material-kit.css')}} rel="stylesheet"/>
 </head>
 
 <body>
 <!-- Navbar Transparent -->
 <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3 navbar-transparent">
     <div class="container">
-        <a class="navbar-brand  text-white " href="{{ route('home') }}" rel="tooltip" title="Designed and Coded by Deiby P." data-placement="bottom">
+        <a class="navbar-brand  text-white " href="{{ route('home') }}" rel="tooltip"
+           title="Designed and Coded by Deiby P." data-placement="bottom">
             Ecommerce Example
         </a>
-        <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
+                aria-label="Toggle navigation">
         <span class="navbar-toggler-icon mt-2">
           <span class="navbar-toggler-bar bar1"></span>
           <span class="navbar-toggler-bar bar2"></span>
@@ -33,14 +34,12 @@
         </span>
         </button>
 
-            </ul>
-        </div>
     </div>
 </nav>
 <!-- End Navbar -->
 
 <div class="page-header align-items-start min-vh-100 mb-2" style="background-color: #2b2b2b;" loading="lazy">
-{{--    <span class="mask bg-gradient-dark opacity-6"></span>--}}
+    {{--    <span class="mask bg-gradient-dark opacity-6"></span>--}}
     <br><br>
     <div class="container my-auto mx-auto">
         <div class="row">
@@ -57,7 +56,7 @@
                             @csrf
                             <div class="input-group input-group-outline my-3">
                                 <label class="form-label">Email</label>
-                                <input type="email" class="form-control">
+                                <input type="email" class="form-control bg-outline-secondary">
                             </div>
                             <div class="input-group input-group-outline mb-3">
                                 <label class="form-label">Password</label>
@@ -67,7 +66,25 @@
                                 <button type="button" class="btn bg-gradient-warning w-100 my-1">Iniciar Sesion</button>
                             </div>
                             <div class="text-center">
-                                <a href="{{ route('login.google') }}" class="btn bg-outline-warning w-100 my-1">Continuar con google<svg style="margin-left: 3%; margin-top: -2%" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/><path d="M1 1h22v22H1z" fill="none"/></svg></a>
+                                <a href="{{ route('login.google') }}" class="btn bg-outline-warning w-100 my-1">Continuar
+                                    con google
+                                    <svg style="margin-left: 3%; margin-top: -2%" xmlns="http://www.w3.org/2000/svg"
+                                         height="24" viewBox="0 0 24 24" width="24">
+                                        <path
+                                            d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                                            fill="#4285F4"/>
+                                        <path
+                                            d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                                            fill="#34A853"/>
+                                        <path
+                                            d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                                            fill="#FBBC05"/>
+                                        <path
+                                            d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                                            fill="#EA4335"/>
+                                        <path d="M1 1h22v22H1z" fill="none"/>
+                                    </svg>
+                                </a>
                             </div>
                             <p class="mt-4 text-sm text-center">
                                 No tienes una cuenta? <a href="#" class="text-warning">Crea una</a>
@@ -83,11 +100,14 @@
             <div class="row align-items-center justify-content-lg-between">
                 <div class="col-12 col-md-6 my-auto">
                     <div class="copyright text-center text-sm text-white text-lg-start">
-                        © <script>
+                        ©
+                        <script>
                             document.write(new Date().getFullYear())
-                        </script>,
+                        </script>
+                        ,
                         Elaborado por
-                        <a href="https://www.creative-tim.com" class="font-weight-bold text-white" target="_blank">Deiby P.</a>
+                        <a href="https://www.creative-tim.com" class="font-weight-bold text-white" target="_blank">Deiby
+                            P.</a>
 
                     </div>
                 </div>
@@ -97,13 +117,16 @@
                             <a href="https://www.creative-tim.com" class="nav-link text-white" target="_blank"></a>
                         </li>
                         <li class="nav-item">
-                            <a href="https://www.creative-tim.com/presentation" class="nav-link text-white" target="_blank">About Us</a>
+                            <a href="https://www.creative-tim.com/presentation" class="nav-link text-white"
+                               target="_blank">About Us</a>
                         </li>
                         <li class="nav-item">
-                            <a href="https://www.creative-tim.com/blog" class="nav-link text-white" target="_blank">Blog</a>
+                            <a href="https://www.creative-tim.com/blog" class="nav-link text-white"
+                               target="_blank">Blog</a>
                         </li>
                         <li class="nav-item">
-                            <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-white" target="_blank">Contact</a>
+                            <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-white"
+                               target="_blank">Contact</a>
                         </li>
                     </ul>
                 </div>
@@ -270,14 +293,14 @@
 {{--</div>--}}
 {{--</div>--}}
 {{--</footer>--}}
+
 <!--   Core JS Files   -->
-<script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
-<script src="../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
-<script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+<script src="{{asset('js/core/popper.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/core/bootstrap.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/plugins/perfect-scrollbar.min.js')}}"></script>
 <!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
-<!--  Google Maps Plugin    -->
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
-<script src="../assets/js/material-kit.min.js?v=3.0.4" type="text/javascript"></script>
+
+<script src="{{asset('js/material-kit.min.js')}}" type="text/javascript"></script>
 </body>
 
 </html>
