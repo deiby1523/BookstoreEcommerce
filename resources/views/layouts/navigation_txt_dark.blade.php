1e63034ea8e9edddcbb87d954011d6b98c10c501
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2024-07-22 22:08:20 -0500

    Fixed a problem connecting to the database

--- resources/views/layouts/navigation_txt_dark.blade.php
+++ resources/views/layouts/navigation_txt_dark.blade.php
@@ -146,11 +146,7 @@
                         <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></svg></span>
 
 
-                        <input id="searchNav" name="searchNav" type="text" class="form-control" placeholder="Buscar Libro">
-
-
-
-                        <!-- TODO: Seguir trabajando en el buscador de libros en la pagina de inicio -->
+                        <input id="searchNav" name="searchNav" type="text" class="form-control" placeholder="Buscar Libro"> <!-- TODO: arreglar el diseño del buscador -->
 
                         <div id="searchResults" name="searchResults">
 
@@ -298,9 +294,11 @@
     // products
     // Event to close the search box if you click outside of it
     document.addEventListener('click', function (e) {
-        if (!searchNav.contains(e.target) && e.target.id !== 'searchNav') {
+        console.log(e.target.id)
+        if (e.target.id !== 'searchNav') {
             var searchResults = document.getElementById('searchResults');
             searchResults.classList.remove('show');
+            searchResults.innerHTML("");
         }
     });
 
