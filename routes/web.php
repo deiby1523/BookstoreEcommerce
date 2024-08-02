7bd8f29514526e20f2fed8b6b6d224ae36c67c4c
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2024-08-01 23:41:56 -0500

    Unnecessary code was removed from the routes

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,6 @@ use App\Http\Controllers\ShoppingCartController;
 use App\Http\Controllers\SubcategoryController;
 use Illuminate\Support\Facades\Route;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', [HomeController::class, 'show'])->name('home');
 
 
