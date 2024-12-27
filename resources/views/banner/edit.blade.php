026a4f0e9be64c115beb900a70a0b8e1b076d85f
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2024-12-27 11:08:36 -0500

    Banner edit

--- resources/views/banner/edit.blade.php
+++ resources/views/banner/edit.blade.php
@@ -3,7 +3,7 @@
 
 
 <head>
-    <title>Crear Categoría</title>
+    <title>Editar Banner</title>
     <!-- Required meta tags -->
     <meta charset="UTF-8">
 
@@ -34,7 +34,7 @@
 <div style="" class="card card-body shadow-xl mt-n12 mx-3 mx-md-4">
     <div class="row mt-4">
         <div class="col-md-3">
-            <a class="btn bg-white mb-0 mt-lg-auto w-100" href="{{route('category.index')}}" class="btn bg-gradient-faded-secondary" style="max-width: 233px; width: -webkit-fill-available;"><svg style="margin-right: 1rem" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+            <a class="btn bg-white mb-0 mt-lg-auto w-100" href="{{route('banner.index')}}" class="btn bg-gradient-faded-secondary" style="max-width: 233px; width: -webkit-fill-available;"><svg style="margin-right: 1rem" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
                 </svg>Volver
             </a>
@@ -43,67 +43,74 @@
     <div class="container">
         <div class="section text-left my-4">
 
-            <h2 class="title">Crear Categoría</h2>
+            <h2 class="title">Editar Banner</h2>
 
             <div class="card">
 
                 <div class="card d-flex justify-content-center p-4 shadow-lg">
                     <form role="form" id="contact-form" method="POST" autocomplete="off"
-                          action="{{ route('category.save') }}" enctype="multipart/form-data">
+                          action="{{ route('banner.update',$banner->id) }} " enctype="multipart/form-data">
                         @csrf
+                        @method('PUT')
                         <div class="card-body pb-2">
                             <div class="row">
-
                                 <div class="col-md-12">
                                     <div class="input-group input-group-static mb-4">
                                         <label>Nombre</label>
-                                        @if(count($errors->get('category_name')) >= 1)
-                                            <input name="category_name" id="category_name" class="form-control"
-                                                   placeholder="Nombre de la categoria" aria-label="Full Name"
+                                        @if(count($errors->get('banner_name')) >= 1)
+                                            <input value="{{$banner->banner_name}}" name="banner_name"
+                                                   id="banner_name" class="form-control"
+                                                   placeholder="Nombre de anuncio" aria-label="Full Name"
                                                    type="text"
-                                                   style="box-shadow: 0 0 8px 2px #ff000061; border-radius: 10px !important;"
-                                                   value="{{app('request')->old('category_name', null)}}">
+                                                   style="box-shadow: 0 0 8px 2px #ff000061; border-radius: 10px !important;">
                                         @else
-                                            <input name="category_name" id="category_name" class="form-control"
-                                                   placeholder="Nombre de la categoria" aria-label="Full Name"
-                                                   type="text" value="{{app('request')->old('category_name', null)}}">
+                                            <input value="{{$banner->banner_name}}" name="banner_name"
+                                                   id="banner_name" class="form-control"
+                                                   placeholder="Nombre del anuncio" aria-label="Full Name"
+                                                   type="text">
                                         @endif
                                     </div>
                                     <x-input-error class="text-danger"
-                                                   :messages="$errors->get('category_name')"></x-input-error>
+                                                   :messages="$errors->get('banner_name')"></x-input-error>
                                 </div>
 
                             </div>
-                            <div class="input-group input-group-static mb-0 mt-md-0 mt-4">
-                                <label>Descripcion</label>
-                                @if(count($errors->get('category_description')) >= 1)
-                                    <textarea name="category_description" class="form-control" id="category_description"
-                                              rows="6"
-                                              placeholder="Una descripcion breve de la categoria"
-                                              style="box-shadow: 0 0 8px 2px #ff000061; border-radius: 10px !important;">{{app('request')->old('category_description', null)}}</textarea>
-                                @else
-                                    <textarea name="category_description" class="form-control" id="category_description"
-                                              rows="6"
-                                              placeholder="Una descripcion breve de la categoria">{{app('request')->old('category_description', null)}}</textarea>
-                                @endif
-                            </div>
-                            <x-input-error class="text-danger"
-                                           :messages="$errors->get('category_description')"></x-input-error>
 
+                            <div class="form-check form-switch py-3">
+                                @if(count($errors->get('active')) >= 1)
+                                    @if($banner->active)
+                                        <input class="form-check-input checked:true" type="checkbox" id="active"
+                                               name="active" checked>
+                                    @else
+                                        <input class="form-check-input checked:false" type="checkbox" id="active"
+                                               name="active">
+                                    @endif
+                                @else
+                                    @if($banner->active)
+                                        <input class="form-check-input checked:true" type="checkbox" id="active"
+                                               name="active" checked>
+                                    @else
+                                        <input class="form-check-input checked:false" type="checkbox" id="active"
+                                               name="active">
+                                    @endif
+                                @endif
+                                <label class="form-check-label" for="active">Activado</label>
+                                <x-input-error class="text-danger"
+                                               :messages="$errors->get('active')"></x-input-error>
+                            </div>
 
                             <div class="row">
-                                <div class="card mt-5" <?php if (count($errors->get('category_image')) >=1 ) { echo ("style='box-shadow: 0 0 8px 2px #ff000061;'");} else { echo("style='box-shadow: 0 5px 15px -3px rgb(0 0 0 / 26%), 0 -4px 6px -2px rgb(0 0 0 / 5%) !important;'");} ?>>
+                                <div class="card mt-5" <?php if (count($errors->get('banner_image')) >=1 ) { echo ("style='box-shadow: 0 0 8px 2px #ff000061;'");} else { echo("style='box-shadow: 0 5px 15px -3px rgb(0 0 0 / 26%), 0 -4px 6px -2px rgb(0 0 0 / 5%) !important;'");} ?>>
                                     <div class="row">
                                         <!-- Card body -->
-                                        <div class="col" style="min-width: 250px">
+                                        <div class="col-5">
                                             <div class="card-body">
                                                 <h4 class="font-weight-normal mt-3">Imagen</h4>
                                                 <p class="card-text mb-4">Subir imagen con formato .jpg, Asegúrate de
-                                                    que la imagen cumpla con una
-                                                    relación de aspecto de 5:6 para garantizar una visualización óptima.
+                                                    que la imagen sea ancha y de buena resolución.
                                                 </p>
                                                 <x-input-error class="text-danger"
-                                                               :messages="$errors->get('category_image')"></x-input-error>
+                                                               :messages="$errors->get('banner_image')"></x-input-error>
                                                 <div class="row mt-5">
                                                     <div class="col-md-4">
 
@@ -118,11 +125,11 @@
                                             </div>
                                         </div>
                                         <!-- Card image -->
-                                        <div class="col mb-4 text-center" style="min-width: 50%">
-                                            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2"
+                                        <div class="col-7 mb-4 text-center">
+                                            <div class="card-header p-0 position-relative mt-n4 z-index-2"
                                                  style="background: none;">
-                                                <img id="category_img" class="border-radius-lg w-50"
-                                                     src="" alt="">
+                                                <img id="banner_img" class="border-radius-lg w-100"
+                                                     src="{{asset($banner->banner_image_url)}}" alt="">
                                             </div>
                                         </div>
                                     </div>
@@ -130,20 +137,17 @@
                             </div>
 
                             <div class="input-group input-group-static mb-4 mt-4">
-                                <input id="fileinput" name="category_image" type="file" accept=".jpg,.jpeg,.png,.webp"
+                                <input id="fileinput" name="banner_image" type="file" accept=".jpg,.jpeg,.png,.webp"
                                        style="display:none;">
                             </div>
-
                             <div class="row">
-                                <div class="col-sm-6 text-start">
-                                    <a href="{{route('category.index')}}" class="btn bg-gradient-danger mt-3 mb-0"
-                                       style="max-width: 233px; width: -webkit-fill-available;">Cancelar
+                                <div class="col-md-6 text-start">
+                                    <a href="{{route('banner.index')}}" class="btn bg-gradient-danger mt-3 mb-0">Cancelar
                                     </a>
                                 </div>
 
-                                <div class="col-sm-6 text-end">
-                                    <button type="submit" class="btn bg-gradient-warning mt-3 mb-0"
-                                            style="max-width: 233px;width: -webkit-fill-available;">Crear
+                                <div class="col-md-6 text-end">
+                                    <button type="submit" class="btn bg-gradient-warning mt-3 mb-0">Guardar
                                     </button>
                                 </div>
                             </div>
@@ -156,6 +160,12 @@
 
 </div>
 
+<div class="container">
+    <div class="row">
+
+    </div>
+</div>
+
 <script>
     $(document).ready(function () {
         $('#falseinput').click(function () {
@@ -166,12 +176,11 @@
         $('#selected_filename').text($('#fileinput')[0].files[0].name);
         let reader = new FileReader();
         reader.onload = (e) => {
-            $('#category_img').attr('src', e.target.result);
+            $('#banner_img').attr('src', e.target.result);
         }
         reader.readAsDataURL(this.files[0]);
     });
 </script>
-
 
 <script src="{{asset('js/core/popper.min.js')}}" type="text/javascript"></script>
 <script src="{{asset('js/core/bootstrap.min.js')}}" type="text/javascript"></script>
