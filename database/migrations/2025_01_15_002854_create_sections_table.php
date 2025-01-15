d3f9fef931ded83c7edd5a7aebd27fa3bb5ea3e8
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-01-14 21:02:55 -0500

    The migration was edited for the sections table

--- database/migrations/2025_01_15_002854_create_sections_table.php
+++ database/migrations/2025_01_15_002854_create_sections_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('sections', function (Blueprint $table) {
             $table->id();
+            $table->string('section_name',100)->nullable(false);
+            $table->string('section_main_title')->nullable();
+            $table->string('section_secondary_title')->nullable();
+            $table->string('section_sub_title')->nullable();
+            $table->string('section_secondary_sub_title')->nullable();
+            $table->text('section_text_1')->nullable();
+            $table->text('section_text_2')->nullable();
+            $table->string('section_color')->nullable(false);
+            $table->string('section_image_1_url',255)->nullable()->unique();
+            $table->string('section_image_2_url',255)->nullable()->unique();
+            $table->string('section_btn_link',255)->nullable();
+            $table->integer('section_style')->nullable(false)->default(0);
             $table->timestamps();
         });
     }
