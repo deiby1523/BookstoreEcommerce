749d391fc9ddbe4af82dfd722cf554d4c1d0e954
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-01-14 21:07:37 -0500

    The controller for section was created

---
+++ app/Http/Controllers/SectionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Section;
+use Illuminate\Http\Request;
+
+class SectionController extends Controller
+{
+    public function index() {
+        $sections = Section::all();
+        return view('sections.index', compact('sections'));
+    }
+
+}
