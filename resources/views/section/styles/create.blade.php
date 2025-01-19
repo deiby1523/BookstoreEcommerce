f6433107677e20e20bcfbf9cfbf1cbf6fb282e69
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-01-19 15:13:23 -0500

    Styles of section create view

---
+++ resources/views/section/styles/create.blade.php
@@ -0,0 +1,75 @@
+<style>
+
+    /* Warning */
+    .btn-outline-warning:hover:not(.active) {
+        background-color: #fb8c00;
+        opacity: 1;
+        box-shadow: none;
+        color: white;
+    }
+
+    .btn-check:checked + .btn-outline-warning {
+        color: white;
+    }
+
+    /* Success */
+    .btn-outline-success:hover:not(.active) {
+        background-color: #4CAF50;
+        opacity: 1;
+        box-shadow: none;
+        color: white;
+    }
+
+    .btn-check:checked + .btn-outline-success {
+        color: white;
+    }
+
+    /*  Danger  */
+    .btn-outline-danger:hover:not(.active) {
+        background-color: #F44335;
+        opacity: 1;
+        box-shadow: none;
+        color: white;
+    }
+
+    .btn-check:checked + .btn-outline-danger {
+        color: white;
+    }
+
+    /* Info */
+    .btn-outline-info:hover:not(.active) {
+        background-color: #1A73E8;
+        opacity: 1;
+        box-shadow: none;
+        color: white;
+    }
+
+    .btn-check:checked + .btn-outline-info {
+        color: white;
+    }
+
+    /* Primary */
+    .btn-outline-primary:hover:not(.active) {
+        background-color: #e91e63;
+        opacity: 1;
+        box-shadow: none;
+        color: white;
+    }
+
+    .btn-check:checked + .btn-outline-primary {
+        color: white;
+    }
+
+    /* Secondary */
+    .btn-outline-secondary:hover:not(.active) {
+        background-color: #7b809a;
+        opacity: 1;
+        box-shadow: none;
+        color: white;
+    }
+
+    .btn-check:checked + .btn-outline-secondary {
+        color: white;
+    }
+
+</style>
