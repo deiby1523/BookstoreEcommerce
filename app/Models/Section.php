835e8061bbb6fdb4c554da9c0dcff741d69ac19b
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-01-23 19:08:52 -0500

    Static elimination method in the Section model

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -4,11 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
 
 class Section extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Escuchamos el evento "deleting" del modelo Category
+        static::deleting(function ($section) {
+
+            // Delete the image associated with the section
+            $image1Path = public_path($section->section_image_1_url);
+
+            if (File::exists($image1Path)) {
+                File::delete($image1Path);
+            }
+
+            $image2Path = public_path($section->section_image_2_url);
+
+            if (File::exists($image2Path)) {
+                File::delete($image2Path);
+            }
+
+        });
+    }
 
 }
