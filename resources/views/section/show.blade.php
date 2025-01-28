4d29ce355c65536594d7f4e01677183662fc5d22
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-01-27 20:14:24 -0500

    Implemented section style 3

--- resources/views/section/show.blade.php
+++ resources/views/section/show.blade.php
@@ -143,34 +143,27 @@ $color = "";
                             <div class="mt-5 position-relative">
                                 <div class="container-fluid">
                                     <div class="row">
-                                        <div class="col-10 mx-auto bg-gradient-dark border-radius-lg">
+                                        <div class="col-10 mx-auto bg-gradient-dark border-radius-lg" style="box-shadow: #0000005e -9px 20px 29px 0px;">
                                             <div class="row py-5">
-                                                <div class="col-xl-4 col-md-6 px-5 position-relative">
+                                                <div class="col-xl-4 col-md-6 px-5 position-relative" style="align-content: center">
                                                     <img
-                                                        class="img border-radius-md max-width-300 w-100 position-relative z-index-2 mt-n7"
-                                                        src="https://images.unsplash.com/photo-1521668576204-57ae3afee860?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80"
+                                                        class="img border-radius-md max-width-300 w-100 position-relative z-index-2"
+                                                        src="{{asset($section->section_image_1_url)}}"
                                                         loading="lazy" alt="card image">
                                                 </div>
                                                 <div
                                                     class="col-xl-4 col-md-5 z-index-2 position-relative px-md-3 px-5 my-md-auto mt-4">
-                                                    <i class="material-symbols-rounded text-white text-5xl">format_quote</i>
+                                                    <i class="material-symbols-rounded text-white text-5xl">{{$section->section_main_title}}</i>
                                                     <p class="text-lg text-white">
-                                                        Decisions: If you can’t decide, the answer is no.
-                                                        If two equally difficult paths, choose the one more
-                                                        painful in the short term (pain avoidance is creating
-                                                        an illusion of equality). Choose the path that leaves
-                                                        you more equanimous in the long term.
+                                                        {{$section->section_text_1}}
                                                     </p>
-                                                    <p class="text-white font-weight-bold text-sm">Michael - <span
-                                                            class="text-xs font-weight-normal">Writter</span></p>
-                                                    <hr class="vertical start-100 ms-n5 d-xl-block d-none">
+
                                                 </div>
                                                 <div class="col-1"></div>
                                                 <div class="col-xl-2 col-12 px-xl-0 px-5 my-xl-auto">
-                                                    <h3 class="text-white mt-xl-0 mt-5">1,679,700 +</h3>
-                                                    <p class="text-sm text-white opacity-8">Developers and Companies
-                                                        around the world using our products.</p>
-                                                    <a href="javascript:;" class="text-white icon-move-right text-sm">See
+                                                    <h3 class="text-white mt-xl-0 mt-5">{{$section->section_sub_title}}</h3>
+                                                    <p class="text-sm text-white opacity-8">{{$section->section_text_2}}</p>
+                                                    <a href="{{$section->section_btn_link}}" class="text-white icon-move-right text-sm">See
                                                         all products
                                                         <i class="fas fa-arrow-right text-xs ms-1"></i>
                                                     </a>
