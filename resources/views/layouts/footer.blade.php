aaaf802ace3f9e6936084e3e2c10c0ddf2e06f8f
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-01-29 21:02:08 -0500

    Changed the footer to an external file

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,87 @@
+<footer class="footer pt-5 mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 mb-4 ms-auto">
+                <div><a href=""><img src="{{asset('img/logos/Home_Logo.png')}}" style="max-width: 10rem !important;"
+                                     class="mb-3 footer-logo" alt="main_logo"></a><h6
+                        class="font-weight-bolder mb-4">Librería y más.</h6></div>
+                <div>
+                    <!-- TODO:Revisar el tema de redes sociales -->
+                    <ul class="d-flex flex-row ms-n3 nav">
+                        <li class="nav-item"><a class="nav-link pe-1" href="https://www.facebook.com/CreativeTim/"
+                                                target="_blank"><i
+                                    class="fab fa-facebook text-lg opacity-8"></i></a></li>
+                        <li class="nav-item"><a class="nav-link pe-1" href="https://twitter.com/creativetim"
+                                                target="_blank"><i class="fab fa-twitter text-lg opacity-8"></i></a>
+                        </li>
+                        <li class="nav-item"><a class="nav-link pe-1" href="https://dribbble.com/creativetim"
+                                                target="_blank"><i
+                                    class="fab fa-dribbble text-lg opacity-8"></i></a></li>
+                        <li class="nav-item"><a class="nav-link pe-1" href="https://github.com/creativetimofficial"
+                                                target="_blank"><i class="fab fa-github text-lg opacity-8"></i></a>
+                        </li>
+                        <li class="nav-item"><a class="nav-link pe-1"
+                                                href="https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w"
+                                                target="_blank"><i class="fab fa-youtube text-lg opacity-8"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-6 col-6 mb-4"><h6 class="text-sm">Organización</h6>
+                <ul class="flex-column ms-n3 nav">
+                    <li class="nav-item"><a class="nav-link" href=""
+                                            target="_blank">Sobre Nosotros</a></li>
+                    <li class="nav-item"><a class="nav-link" href=""
+                                            target="_blank">Servicios</a></li>
+                    <li class="nav-item"><a class="nav-link" href=""
+                                            target="_blank">Nuestra Misión</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-2 col-sm-6 col-6 mb-4"><h6 class="text-sm">Ayuda y Soporte</h6>
+                <ul class="flex-column ms-n3 nav">
+                    <li class="nav-item"><a class="nav-link" href=""
+                                            target="_blank">Contáctanos</a></li>
+                    <li class="nav-item"><a class="nav-link" href=""
+                                            target="_blank">Sugerencias</a></li>
+                    <li class="nav-item"><a class="nav-link" href=""
+                                            target="_blank">Reportar Error</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 col-sm-6 col-6 mb-4"><h6 class="text-sm">legal</h6>
+                <ul class="flex-column ms-n3 nav">
+                    <li class="nav-item"><a class="nav-link" href=""
+                                            target="_blank">Términos y Condiciones</a></li>
+                    <li class="nav-item"><a class="nav-link" href=""
+                                            target="_blank">Política de Privacidad</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 col-sm-6 col-6 mb-4">
+                <ul class="flex-column ms-n3 nav">
+                    <li class="nav-item">
+                        Carrera 19B #8-41
+                    </li>
+                    <li class="nav-item">
+                        Bucaramanga, Santander
+                    </li>
+                    <li class="nav-item">
+                        deibyfabianpradaquintero@gmail.com
+                    </li>
+                    <li class="nav-item">
+                        cuentasdeiby@gmail.com
+                    </li>
+                </ul>
+            </div>
+            <div class="col-12">
+                <div class="text-center">
+                    <p class="text-dark my-4 text-sm font-weight-normal"> Todos los derechos reservados.
+                        Copyright ©
+                        <script>
+                            document.write(new Date().getFullYear())
+                        </script>
+                        .Librería Vida
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
