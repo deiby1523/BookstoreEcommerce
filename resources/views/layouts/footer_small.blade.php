b47204cfb72ce9576abed513bc710ea4de375078
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-01-29 21:02:27 -0500

    A small footer was created

---
+++ resources/views/layouts/footer_small.blade.php
@@ -0,0 +1,38 @@
+<footer class="footer position-absolute bottom-2 py-2 w-100">
+    <div class="container">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-12 col-md-6 my-auto">
+                <div class="copyright text-center text-sm text-dark text-lg-start">
+                    ©
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script>
+                    ,
+                    Elaborado por
+                    <a href="" class="font-weight-bold text-dark" target="_blank">Deiby
+                        P.</a>
+
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                    <li class="nav-item">
+                        <a href="https://www.creative-tim.com" class="nav-link text-dark" target="_blank"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.creative-tim.com/presentation" class="nav-link text-dark"
+                           target="_blank">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.creative-tim.com/blog" class="nav-link text-dark"
+                           target="_blank">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-dark"
+                           target="_blank">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
