8de8dad2f22af84dd236f6c013a70d1df05e2b19
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-02-02 14:10:02 -0500

    The Product dashboard controller was created

---
+++ app/Http/Controllers/ProductDashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Category;
+use App\Models\Publisher;
+use App\Models\Subcategory;
+use Illuminate\Http\Request;
+
+class ProductDashboardController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        $subcategories = Subcategory::all();
+        return view('products_dashboard', compact('categories','subcategories'));
+    }
+}
