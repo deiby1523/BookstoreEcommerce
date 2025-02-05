1828646afd8d4dda77768869ede7088719545854
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-02-05 10:11:37 -0500

    The product controller was updated

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,31 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $products = Product::all();
-        return view('products.index', compact('products'));
+        return view('product.index', compact('products'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        return view('products.create');
+        $categories = Category::all();
+        return view('product.create', compact('categories'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+
+    public function save(Request $request)
     {
         $request->validate([
             'name' => 'required',
