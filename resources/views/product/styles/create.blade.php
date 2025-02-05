74aee247446c6d755102b0cefba7b19b952b9e2f
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-02-05 17:55:19 -0500

    Create product view was implemented

---
+++ resources/views/product/styles/create.blade.php
@@ -0,0 +1,42 @@
+<style>
+    .listbox {
+        margin-top: 10px !important;
+    }
+
+
+    input[type=number]::-webkit-inner-spin-button,
+    input[type=number]::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    input[type=number] {
+        -moz-appearance: textfield;
+    }
+
+
+    input[type=date]::-webkit-inner-spin-button,
+    input[type=date]::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    input[type=date] {
+        -moz-appearance: textfield;
+    }
+
+    .selectSearch {
+        display: none;
+    }
+
+    .show {
+        display: block;
+    }
+
+    input::file-selector-button {
+        padding: 7px 7px 7px 7px !important;
+        background-color: lightgray !important;
+        border-radius: 10px;
+    !important
+    }
+</style>
