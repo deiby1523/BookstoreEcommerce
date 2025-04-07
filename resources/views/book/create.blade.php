ff41cf1b1814508a0a2d99f2e7fbeb9def8fe5fb
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-04-07 18:02:46 -0500

    Added a back button in create and edit

--- resources/views/book/create.blade.php
+++ resources/views/book/create.blade.php
@@ -28,12 +28,18 @@
 
 {{-- aditional styles--}}
 @include('book.styles.create')
-
 @include('layouts.sidebar')
-
 @include('layouts.header')
 
 <main>
+    <div class="row mt-4">
+        <div class="col-md-3">
+            <a class="btn bg-transparent mb-0 mt-lg-auto" href="{{route('book.index')}}"><svg style="margin-right: 1rem" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+                </svg>Volver
+            </a>
+        </div>
+    </div>
     <div class="container">
 
         <div class="section text-left my-4">
