1f58a16d5beb4ec11986e53809a0d5e7e552b69f
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-04-06 22:54:10 -0500

    A template with a sidebar and header was developed for the dashboard.

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+<p class="mb-0 text-2xl">Panel Principal</p>
+    <div>
+        <a class="link link-dark cursor-pointer">Cerrar sesión
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#344767"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg>
+        </a>
+    </div>
+
+</header>
